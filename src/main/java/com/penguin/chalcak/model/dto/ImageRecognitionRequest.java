8487937219fc11946f145ff4d79268cfbb986134
roasted-penguin <swkwon25@hanyang.ac.kr>
2024-01-23 17:56:22 +0900

    feat : ImageRecognition dto 구체화

--- src/main/java/com/penguin/chalcak/model/dto/ImageRecognitionRequest.java
+++ src/main/java/com/penguin/chalcak/model/dto/ImageRecognitionRequest.java
@@ -13,6 +13,7 @@ public class ImageRecognitionRequest {
     private String requestId;
     private long timestamp;
     private String lang;
+    //list 형식이지만 현재는 한개만 허용 20240123
     private List<Image> images;
     private boolean enableTableDetection;
 
@@ -24,6 +25,7 @@ public class ImageRecognitionRequest {
         private String url;
         private String data;
         private String name;
+        //텍스트 OCR API에서는 사용되지 않음
         //private JSONArray templateIds;
     }
 }
--- src/main/java/com/penguin/chalcak/model/dto/ImageRecognitionResponse.java
+++ src/main/java/com/penguin/chalcak/model/dto/ImageRecognitionResponse.java
@@ -1,14 +1,62 @@
 package com.penguin.chalcak.model.dto;
 
+import java.util.List;
+
 import lombok.*;
 
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 public class ImageRecognitionResponse {
     //CLOVA OCR V2
-
-
+	private String uid;
+	private String name;
+	private String inferResult;
+	private String message;
+	private MatchedTemplate matchedTemplate;
+	private String title;
+	private List<Field> fields;
+	private ValidationResult validationResult;
+	
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor
+	@Builder
+	public static class MatchedTemplate{
+		private int id;
+		private String name;
+	}
+	
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor
+	@Builder
+	public static class Field{
+		private String name;
+		private String valueType;
+		private String inferText;
+		private double inferConfidence;
+		private Bounding bounding;
+	}
+	
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor
+	@Builder
+	public static class Bounding{
+		private double top;
+		private double left;
+		private double width;
+		private double height;
+	}
+	
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Builder
+    public static class ValidationResult {
+        private String result;
+        private String message;
+    }
 }
